4b138848a3d954601a98f5ca5c44358f13c2861a
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-09-15 21:19:59 +0900

    feat: add search-result-card

---
+++ src/app/components/search-result-card/search-result-card.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchResultCardComponent } from './search-result-card.component';
+
+describe('SearchResultCardComponent', () => {
+  let component: SearchResultCardComponent;
+  let fixture: ComponentFixture<SearchResultCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SearchResultCardComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchResultCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
