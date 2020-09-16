b03b214d905647ff195d534e0e3b9821201851bb
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-09-16 20:46:45 +0900

    feat: update card design with @angular/material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GoogleSearchGetterService, SearchResult } from './services/google-search-getter.service';
 
 @Component({
@@ -6,11 +6,16 @@ import { GoogleSearchGetterService, SearchResult } from './services/google-searc
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'uni-search';
   searchResults: SearchResult[] = [];
 
   constructor(private readonly googleSearchGetterService: GoogleSearchGetterService) {}
+
+  ngOnInit() {
+    // to develop css
+    this.onSearchBoxSubmit('test');
+  }
 
   async onSearchBoxSubmit(text: string) {
     this.searchResults = await this.googleSearchGetterService.search(text);
