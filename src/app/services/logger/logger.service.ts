6a02a111d20a1c0d2ced5fb1f60a825396aa9137
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-09-22 17:34:53 +0900

    feat: add LoggerService (#11)

---
+++ src/app/services/logger/logger.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+type LogData = {
+  time: number;
+  type: 'debug' | 'info' | 'error';
+  data: unknown;
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoggerService {
+  private logStream = new Subject<LogData>();
+
+  get logs() {
+    return this.logStream.asObservable();
+  }
+
+  debug(...args: unknown[]) {
+    this.log('debug', args);
+  }
+
+  info(...args: unknown[]) {
+    this.log('info', args);
+  }
+
+  error(...args: unknown[]) {
+    this.log('error', args);
+  }
+
+  private log(type: LogData['type'], data: unknown[]) {
+    this.logStream.next({
+      time: Date.now(),
+      type,
+      data,
+    });
+  }
+}
