8b50b1fd10ea38be7ae41420dd4918c6ff92b351
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-10-05 22:36:33 +0900

    feat: show favicons in search result

--- src/app/components/search-result-card/search-result-card.component.ts
+++ src/app/components/search-result-card/search-result-card.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
 import { SearchResult } from '../../models/core';
 import { emptyResult } from '../../services/google/extract';
+
+const getGoogleFaviconApi = (domain: string) => `https://www.google.com/s2/favicons?domain=${domain}`;
 
 @Component({
   selector: 'app-search-result-card',
@@ -8,6 +10,10 @@ import { emptyResult } from '../../services/google/extract';
   styleUrls: ['./search-result-card.component.scss'],
 })
 export class SearchResultCardComponent {
+  get iconSrc() {
+    return getGoogleFaviconApi(this.data.domain);
+  }
+
   @Input() data: SearchResult = emptyResult;
   @Output() clicked = new EventEmitter<SearchResult>();
   @HostListener('click') click() {
